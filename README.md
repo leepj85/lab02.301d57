f79e9d252dd9aaa556037a88dd939c27b2a4a9d7
trevorjdobson <trevorjdobson@gmail.com>
2019-07-05 16:53:30 -0700

    we both drove and navigated via liveshare, everything is working?

--- README.md
+++ README.md
@@ -27,3 +27,38 @@ Peter Lee, Brandon Hurrington, and Trevor Dobson
 * Actual time needed to complete: 1hr
 * Driver: Trevor
 * Navigators: Brandon & Peter
+
+4. Templating
+* Estimate of time needed to complete: 30 mins
+* Start time: 1:30pm
+* Finish time: 2:00pm
+* Actual time needed to complete: 30 mins
+* Driver: Trevor
+* Navigators: Brandon 
+
+5. Pagination
+* Estimate of time needed to complete: 30 mins
+* Start time: 2:00pm
+* Finish time: 4:15pm
+* Actual time needed to complete: 2 hrs 15 mins
+* Driver: Brandon
+* Navigators: Trevor
+
+6. Sort the Images
+* Estimate of time needed to complete: 30 mins
+* Start time: 4:15pm
+* Finish time: 4:45pm
+* Actual time needed to complete: 30 mins
+* Driver: Brandon
+* Navigators: Trevor
+
+7. Styling with Flexbox
+* Estimate of time needed to complete: 30 mins
+* Start time: 4:45pm
+* Finish time: 5:00pm
+* Actual time needed to complete: 15 mins
+* Driver: Trevor
+* Navigators: Brandon
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
 'use strict'
 
+let page = {
+  pageNumber: 1,
+}
+
+let pageView = function() {
+  if (page.pageNumber === 1) {
+    $('section').hide();
+    $('.page-one-image').show();
+    $('#dropdown').html('');
+    Image.pageOneImages.forEach(img => {
+      img.dropdownBuilder();
+    })
+  }
+  if (page.pageNumber === 2) {
+    $('section').hide();
+    $('.page-two-image').show();
+    $('#dropdown').html('');
+    Image.pageTwoImages.forEach(img => {
+      img.dropdownBuilder();
+    })
+  }
+};
+
+let toggleView = (number) => {
+  page.pageNumber = number;
+  pageView();
+};
+
 // CONSTRUCTOR for images and adds it to the array of images.
 function Image(img) {
   this.url = img.image_url;
@@ -7,54 +35,97 @@ function Image(img) {
   this.description = img.description;
   this.keyword = img.keyword;
   this.horns = img.horns;
+  this.page = img.page;
 }
 
-Image.allImages = [];
-
+Image.pageOneImages = [];
+Image.pageTwoImages = [];
 
 // jQuery GETTER for .json  
 Image.readJson = () => {
   $.get('data/page-1.json', 'json')
     .then(data => {
       data.forEach(img => {
-        Image.allImages.push(new Image(img));
+        img.page = 'page-one-image'
+        Image.pageOneImages.push(new Image(img));
       });
-      Image.allImages.forEach(img => {
+      Image.pageOneImages.sort((a,b)=> a.title.localeCompare(b.title));
+      Image.pageOneImages.forEach(img => {
         img.render();
-        img.dropdownBuilder();
+        pageView();
       })
     })
+  $.get('data/page-2.json', 'json')
+    .then(data => {
+      data.forEach(img => {
+        img.page = 'page-two-image'
+        Image.pageTwoImages.push(new Image(img));
+      });
+      Image.pageTwoImages.sort((a,b)=> a.title.localeCompare(b.title));
+      Image.pageTwoImages.forEach(img => {
+        img.render();
+        pageView();
+      })
+    })
+  
 };
 
 // Renders the images to the screen
-Image.prototype.render = function() {
+Image.prototype.render = function () {
   // New image section using jQuery 
   const $source = $('#handlebars-template').html();
   const template = Handlebars.compile($source);
   const newHtml = template(this);
   $('main').append(newHtml);
-
 }
 
 // Populates the dropdown selector
-Image.prototype.dropdownBuilder = function() {
+Image.prototype.dropdownBuilder = function () {
   const $dropdown = $('#dropdown');
-
   // Code cited at bottom.
   if ($(`#dropdown option[value='${this.keyword}']`).length <= 0) {
     $dropdown.append($('<option />').val(this.keyword).text(this.keyword));
   }
+  
 };
 
 // Dropdown click handler that manages filtering.
-$("#dropdown").on('change', function() { 
+$("#dropdown").on('change', function () {
   $('section').hide();
   if (this.value === 'default') {
     $('section').show();
     $('#photo-template').hide();
   }
+  
   $(`.${this.value}`).show();
 });
+
+function sortByAlpha(){
+  $('main').html('');
+  Image.pageOneImages.sort((a,b)=> a.title.localeCompare(b.title));
+  Image.pageOneImages.forEach(img => {
+    img.render();
+  })
+  Image.pageTwoImages.sort((a,b)=> a.title.localeCompare(b.title));
+  Image.pageTwoImages.forEach(img => {
+    img.render();
+  })
+  pageView();
+}
+
+function sortByHorns(){
+  $('main').html('');
+  Image.pageOneImages.sort((a,b)=> a.horns - b.horns);
+  Image.pageOneImages.forEach(img => {
+    img.render();
+  })
+  Image.pageTwoImages.sort((a,b)=> a.horns - b.horns);
+  Image.pageTwoImages.forEach(img => {
+    img.render();
+  })
+  pageView();
+}
+
 
 Image.readJson();
 
